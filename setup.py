f427b7bda85d1d547de9d96f580b1656bc26c549
Grant Bakker <grant@bakker.pw>
2019-05-01 17:17:20 -0400

    Add path hook to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,49 @@
 # -*- coding: utf-8 -*-
+import distutils
+import os.path
+
 from setuptools import setup
+from setuptools.command.install import install as setuptools_install
+
+
+PTH = """
+try:
+    import source_tranform
+except ImportError:
+    pass
+else:
+    source_tranform.register()
+"""
+
+
+class install(setuptools_install):
+
+    def initialize_options(self):
+        setuptools_install.initialize_options(self)
+
+        contents = 'import sys; exec({!r})\n'.format(PTH)
+        self.extra_path = (self.distribution.metadata.name, contents)
+
+    def finalize_options(self):
+        setuptools_install.finalize_options(self)
+
+        install_suffix = os.path.relpath(
+            self.install_lib, self.install_libbase,
+        )
+        if install_suffix == '.':
+            distutils.log.info('skipping install of .pth during easy-install')
+        elif install_suffix == self.extra_path[1]:
+            self.install_lib = self.install_libbase
+            distutils.log.info(
+                "will install .pth to '%s.pth'",
+                os.path.join(self.install_lib, self.extra_path[0]),
+            )
+        else:
+            raise AssertionError(
+                'unexpected install_suffix',
+                self.install_lib, self.install_libbase, install_suffix,
+            )
+
 
 setup(
     name='source-transform',
@@ -15,4 +59,5 @@ setup(
         'License :: OSI Approved :: MIT License',
     ),
     packages=['source_transform'],
+    cmdclass={'install': install},
 )
