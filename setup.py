8c271b4f8a3dd92bc60e8eb8c7f8566c5a0234b3
Grant Bakker <grant@bakker.pw>
2019-05-01 14:42:54 -0400

    Add setup and README

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='source-transform',
+    version='0.0.1',
+    author='Grant Bakker',
+    author_email='grant@bakker.pw',
+    description='Python source transformation framework',
+    url='https://bakkerthehacker.github.io/source-transform/',
+    classifiers=(
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ),
+    packages=['source_transform'],
+)
